e538ce6577264889d2258a66ca0347ef417e76c0
phphardy <48085368+phphardy@users.noreply.github.com>
2019-02-28 19:44:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,12 @@
   phpinfo();
   echo date("Y-m-d H:i:s");
   echo 1;
+  function createAjax(){
+    try{
+      return new XMLHttpResponse();
+    }catch(error){}
+    try{
+      return new ActiveXobject("microsoft.XMLHTTP");
+    }catch(error){}
+  }
 ?>
